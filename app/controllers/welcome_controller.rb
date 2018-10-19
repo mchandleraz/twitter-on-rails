1f1a3d4e702dbd53c6c80db9a66e17969d444541
Matthew Chandler <mcaz689@gmail.com>
2018-10-18 23:50:06 -0700

    add tweet list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
+  def initialize
+    super
+
+    @twitter = TwitterService.new({
+      key: Rails.application.credentials.twitter[:client_key],
+      secret: Rails.application.credentials.twitter[:client_secret]
+    })
+  end
+
   def index
+    @tweets = @twitter.search("reactjs")
   end
 end
